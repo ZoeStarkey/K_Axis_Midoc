5e1f95f74e9aa294ee8e63de8742d73b8f5566fa
RTreb <rowan.trebilco@utas.edu.au>
2017-11-14 14:32:59 +1100

    updates for KP symposium talk

--- midoc_catch_bubbles.R
+++ midoc_catch_bubbles.R
@@ -6,13 +6,11 @@
 ## '/home/shared/data/chlmisc/chl_max_kerg2016/v3_overall.png'
 
 library(raster)
-library(palr)
 library(orsifronts)
 library(rworldxtra)
 data(countriesHigh)
-library(raadtools)
-library(RColorBrewer)
-
+library(dplyr)
+library(tidyr)
 
 # define plot extent
 ext <- extent(60, 100, -70, -50)
@@ -32,34 +30,34 @@ ext2 <- extent(62, 98, -68, -52)
 
 # read in MIDOC data
 # generic function for reading biomass csvs, and coding DNC plot colour
-read.bm<- function(bm.rds){ 
-  bmdat<- readRDS(bm.rds)
-  bmdat$col<- ifelse(bmdat$DNC=="N", "dark blue", 
-                     ifelse(bmdat$DNC=="D", "yellow",
-                            ifelse(bmdat$DNC=="MC", "orange", "violet")))
-  coordinates(bmdat) <- ~lon+lat
-  bmdat
-}
 
-# total biomass for fish
-stn.bm<- read.bm("f.tots.csv")
+crep <- readr::read_csv(("./source data/midoc_crepuscular.csv"))
 
-# combined fish biomass split by codend
-CE.bm<- read.bm("f.CE.tots.csv")
+bm.tax <- readRDS("./derived data/codend_taxa_biomass.rds")
+bm.wide <- bm.tax %>% spread(key=tax.grp, value=bm, fill=0)
 
-# biomass of higher taxonomic groups - total
-grps.bm <- read.bm("stn.tax.bm.tots.csv")
+km <- readRDS("./source data/midoc_stations_locations_times.rds")
+# bm.wide <- bm.wide %>% inner_join(km)
+# bm.tax <- bm.tax %>% inner_join(km)
 
-# total fish biomass split by group - total
-f.grps.bm <- read.bm("stn.bm.tots.csv")
+ktr <- readr::read_csv("./source data/v3_201516030_waypoints_dec.csv")
+colnames(ktr) <- c("wp","lat","lon","wp.grp")
+ktr <- ktr[-1,]
+coordinates(ktr) <- ~lon+lat
 
-# voyage data
-voyage <- readRDS("/home/shared/data/chlmisc/chl_max_kerg2016/v3points.rds")
+naf <- c("TRIAL","MIDOC02","MIDOC08","MIDOC10","MIDOC12","MIDOC13","MIDOC33")
+
+bm.fish  <- bm.tax %>% filter(tax.grp=="fish", midoc.stn%in%naf==F) %>% ungroup() %>% group_by(midoc.stn) %>% summarize(bm.tot = sum(bm, na.rm=T)) %>% inner_join(km) %>% inner_join(crep)
+bm.krill <- bm.tax %>% filter(tax.grp=="krill", midoc.stn%in%naf==F) %>% ungroup() %>% group_by(midoc.stn) %>% summarize(bm.tot = sum(bm, na.rm=T)) %>% inner_join(km) %>% inner_join(crep)
+bm.ceph <- bm.tax %>% filter(tax.grp=="cephalopods", midoc.stn%in%naf==F) %>% ungroup() %>% group_by(midoc.stn) %>% summarize(bm.tot = sum(bm, na.rm=T)) %>% inner_join(km) %>% inner_join(crep)
+bm.jelly <- bm.tax %>% filter(tax.grp=="cnidarians", midoc.stn%in%naf==F) %>% ungroup() %>% group_by(midoc.stn) %>% summarize(bm.tot = sum(bm, na.rm=T)) %>% inner_join(km) %>% inner_join(crep)
+bm.salps <- bm.tax %>% filter(tax.grp=="salps", midoc.stn%in%naf==F) %>% ungroup() %>% group_by(midoc.stn) %>% summarize(bm.tot = sum(bm, na.rm=T)) %>% inner_join(km) %>% inner_join(crep)
+
 
 # function to scale point size consistently for bubble plots
 key.values<- c(0.25, 0.5, 1,2,4,8,16) # breaks for scale
 scale.bubbles<- function(bmdat, maxsize = 4, key = key.values){	
-  abm = bmdat$bm.grand.tot
+  abm = bmdat$bm.tot
   # scale to square root
   abm = sqrt(abm)
   cex = as.vector(maxsize * abm/max(abm, sqrt(key)))    
@@ -70,16 +68,21 @@ scale.bubbles<- function(bmdat, maxsize = 4, key = key.values){
 # plotting
 plotbm <- function(pdf.name, bm.dat, the.main){
 
+  bm.dat$col<- ifelse(bm.dat$DNC.visual=="N", "dark blue", 
+                     ifelse(bm.dat$DNC.visual=="D", "yellow",
+                            ifelse(bm.dat$DNC.visual=="MC", "orange", "violet")))
+  coordinates(bm.dat) <- ~LONGITUDE+LATITUDE
+
   pdf(height=6, width=8, pdf.name)
   par(las=1)
   plot(ext2, ylab="", xlab="", type="n", main=the.main)
-  plot(chl, col = pal$cols, breaks = pal$breaks, legend = FALSE, add=T)
-  plot(crop(orsifronts,ext), add = TRUE, col="#053061", lty=1, lwd=1)
-  plot(spTransform(rasterToContour(icejan[[1]], lev = 15), projection(countriesHigh)), add = TRUE, lty=1, col="#00FFFF", lwd=1.5)
-  plot(spTransform(rasterToContour(icenov[[1]], lev = 15), projection(countriesHigh)), add = TRUE, lty = 1, lwd=0.5)
+  #plot(chl, col = pal$cols, breaks = pal$breaks, legend = FALSE, add=T)
+  #plot(crop(orsifronts,ext), add = TRUE, col="#053061", lty=1, lwd=1)
+  #plot(spTransform(rasterToContour(icejan[[1]], lev = 15), projection(countriesHigh)), add = TRUE, lty=1, col="#00FFFF", lwd=1.5)
+  #plot(spTransform(rasterToContour(icenov[[1]], lev = 15), projection(countriesHigh)), add = TRUE, lty = 1, lwd=0.5)
   plot(countriesHigh, add=T, col="gray90", border="gray70")
   
-  lines(coordinates(voyage), col="gray40", lwd=2)
+  lines(coordinates(ktr), col="gray40", lwd=2)
   
   
   # old plotting, using automatic scaling of bubbles with bubble()
@@ -104,40 +107,12 @@ plotbm <- function(pdf.name, bm.dat, the.main){
   dev.off()
 }
 
-plotbm(pdf.name="k_axis_fish_biomass_total.pdf", bm.dat=stn.bm, the.main="all fish")
-plotbm(pdf.name="k_axis_fish_biomass_CE2.pdf", bm.dat=CE.bm[CE.bm$cod.end==2,], the.main="800-1000m")
-plotbm(pdf.name="k_axis_fish_biomass_CE3.pdf", bm.dat=CE.bm[CE.bm$cod.end==3,], the.main="600-800m")
-plotbm(pdf.name="k_axis_fish_biomass_CE4.pdf", bm.dat=CE.bm[CE.bm$cod.end==4,], the.main="400-600m")
-plotbm(pdf.name="k_axis_fish_biomass_CE5.pdf", bm.dat=CE.bm[CE.bm$cod.end==5,], the.main="200-400m")
-plotbm(pdf.name="k_axis_fish_biomass_CE6.pdf", bm.dat=CE.bm[CE.bm$cod.end==6,], the.main="0-200m")
-
-# fish tax groups - subset from f.grps.bm
-# Bathylagus sp.
-plotbm(pdf.name="k_axis_bathylagus_biomass_total.pdf", bm.dat=subset(f.grps.bm, f.grps.bm$fish.grp=="Bathylagiids"), the.main="Bathylagus sp.")
-# Electrona sp.
-plotbm(pdf.name="k_axis_electrona_biomass_total.pdf", bm.dat=subset(f.grps.bm, f.grps.bm$fish.grp=="Electrona sp."), the.main="Electrona sp.")
-
-# Krefftichthys sp.
-plotbm(pdf.name="k_axis_krefftichthys_biomass_total.pdf", bm.dat=subset(f.grps.bm, f.grps.bm$fish.grp=="Kreffichthys andersonii"), the.main="Krefftichthys sp.")
-
-# Gymnoscopelus sp. 
-plotbm(pdf.name="k_axis_gymnoscopelus_biomass_total.pdf", bm.dat=subset(f.grps.bm, f.grps.bm$fish.grp=="Gymnoscopelus sp."), the.main="Gymnoscopelus sp.")
-
-# Protomyctophum sp. 
-plotbm(pdf.name="k_axis_Protomyctophum_biomass_total.pdf", bm.dat=subset(f.grps.bm, f.grps.bm$fish.grp=="Protomyctophum sp"), the.main="Protomyctophum sp.")
-
-# non-fish groups - subset from grps.bm 
-# cephalopods
-plotbm(pdf.name="k_axis_cephalopod_biomass_total.pdf", bm.dat=subset(grps.bm, grps.bm$tax.grp=="cephalopods"), the.main="Cephalopods")
-
-# cnidarians
-plotbm(pdf.name="k_axis_cnidarian_biomass_total.pdf", bm.dat=subset(grps.bm, grps.bm$tax.grp=="cnidarians"), the.main="Cnidarians")
-
-# krill
-plotbm(pdf.name="k_axis_krill_biomass_total.pdf", bm.dat=subset(grps.bm, grps.bm$tax.grp=="krill"), the.main="Krill")
-
-# salps 
-plotbm(pdf.name="k_axis_salps_biomass_total.pdf", bm.dat=subset(grps.bm, grps.bm$tax.grp=="salps"), the.main="Salps")
-
-# mixed krill and salps
-plotbm(pdf.name="k_axis_mixed_krill_salps_biomass_total.pdf", bm.dat=subset(grps.bm, grps.bm$tax.grp=="mixed krill and salps"), the.main="Mixed krill and salps")
+# bubble plots for
+  # total fish
+    plotbm("fish.pdf", bm.fish, "fish")
+  # total squid
+    plotbm("squid.pdf", bm.ceph, "squid")
+  # total krill
+    plotbm("krill.pdf", bm.krill, "krill")
+  # total salps 
+    plotbm("salps.pdf", bm.salps, "salps")    
