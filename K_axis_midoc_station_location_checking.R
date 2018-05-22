46dd1c0520e9c8cc4a765f71c1228903b3b38a22
RTreb <rowan.trebilco@utas.edu.au>
2018-05-22 15:27:43 +1000

    chip chip chip at check check check

--- K_axis_midoc_station_location_checking.R
+++ K_axis_midoc_station_location_checking.R
@@ -36,6 +36,14 @@ ggplot(data=subset(ktr, ktr$lat< -55), aes(y=lat, x=lon)) +
 
 # onboard logger data from midoc
 md <- readRDS("midoc_data.rds") ## note that something is wrong with the locations here
+# what's happening for the NA deployment on Jan 28th? Might be MIDOC13?
+
+# plotting to check
+ggplot(md, aes(x=datetime, y=Pressure..dbar., colour=status)) + geom_point() + facet_wrap(~deployment, scales = "free_x")
+ggsave("midoc_deployment_pressure.pdf", height=16, width=20)
+
+md$midoc<- 
+
 
 # also have voyage track for reference
 ktr <- read_csv("v3_201516030_waypoints_dec.csv") 
@@ -58,5 +66,16 @@ nav <- readRDS("navigation_1min.rds")
 
 ggplot(ktr, aes(x=lon, y=lat)) + geom_path() + geom_path(data=nav, aes(y=LATITUDE, x=LONGITUDE), col="blue")
 
-smn %>% group_by(station, status) %>% summarise(t.start=first(time), t.end=last(time))
+# start and end for each midoc shot
+md.se <- smn %>% group_by(station) %>% summarise(t.start=first(time), t.end=last(time))
+
+
+# start and end for each codend
+ce.se <- smn %>% group_by(station, status) %>% summarise(t.start=first(time), t.end=last(time))
+
+# super-impose these on the midoc plot from about, laid out linear; make sure all lines up
+ggplot(md %>% filter(filenum>1), aes(x=datetime, y=Pressure..dbar.)) + geom_path() + geom_text(data=md_st, aes(x=datetime, y=500, label=substr(midoc.stn,6,7), col="red")) + 
+	geom_vline(data=md.se, aes(xintercept=t.start),colour="dark green")+
+	geom_vline(data=md.se, aes(xintercept=t.end),colour="red")
+
 
