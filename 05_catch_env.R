cb517366cb7d2189e7fad817baddf4e04568851b
RTreb <rowan.trebilco@utas.edu.au>
2018-07-03 15:48:36 +1000

    data importing and integration steps finished... for now

--- 05_catch_env.R
+++ 05_catch_env.R
@@ -34,175 +34,48 @@ oc <- readRDS("./source data/k_axis_oceanog_summ.Rda")
 dp <- read_csv("./source data/KAXIS_deep_param.csv", col_names=c('station', 'Smax', 'O2_min', 'Tmax', 'Tmin','y','m','d'))
 
 # # compare 'deep parameters' with previous summary info
-# oc <- inner_join(oc, dp)
+oc <- inner_join(oc, dp) %>% select(-Tmin_value)
 # oc$Tmin_value - oc$Tmin # Tmins are the same
 rm(dp)
 
-# linear interpolation of Tmin, mixed layer depths based on time
+# chlorophyll and ice
+chl_ice <- read_rds("./derived data/k-axis-midoc-satdata-extraction_2017-11-09.rds")
+
+## TODO: locations of stations 4 and 5 are a bit off...
+## in the short-term, this can be corrected with linear interpolation as per below; but a better solution would be to re-extract
+## probably immaterial for chlorophyll given the approach; but should be checked for ice
+# ggplot(km, aes(x=lon_start, y=lat_start)) + geom_point() + geom_path(data=nav, aes(x=LONGITUDE, y=LATITUDE)) +geom_point(data=chl_ice, aes(x=LONGITUDE,y=LATITUDE), col="blue", size=3) + geom_text(data=chl_ice, aes(label=substr(midoc.stn, 6,8), x=LONGITUDE, y=LATITUDE))
+
+# linear interpolation of variables based on time
 oc$gmt <- ISOdatetime(2016, oc$month, oc$day, 0, 0, 0, tz = "GMT")
-km$Tmin <- approxfun(oc$gmt, oc$Tmin_value, rule = 2)(km$start_time)
+km$Tmin <- approxfun(oc$gmt, oc$Tmin, rule = 2)(km$start_time)
 km$Tmin_depth <- approxfun(oc$gmt, oc$Tmin_depth, rule = 2)(km$start_time)
 km$Tmax <- approxfun(oc$gmt, oc$Tmax, rule = 2)(km$start_time)
 km$SML <- approxfun(oc$gmt, oc$SML_preferred_estimate, rule = 2)(km$start_time)
 km$Smax <- approxfun(oc$gmt, oc$Smax, rule = 2)(km$start_time)
 km$O2_min <- approxfun(oc$gmt, oc$O2_min, rule = 2)(km$start_time)
+km$days_since_melt <- approxfun(chl_ice$datetime, chl_ice$days_since_melt, rule = 2)(km$start_time)
+km$distance_to_ice_m <- approxfun(chl_ice$datetime, chl_ice$distance_to_ice_m, rule = 2)(km$start_time)
+km$distance_to_edge_m <- approxfun(chl_ice$datetime, chl_ice$distance_to_edge_m, rule = 2)(km$start_time)
+km$sea_ice_conc <- approxfun(chl_ice$datetime, chl_ice$sea_ice_conc, rule = 2)(km$start_time)
+km$sea_ice_conc <- approxfun(chl_ice$datetime, chl_ice$sea_ice_conc, rule = 2)(km$start_time)
+km$chl_rs <- approxfun(chl_ice$datetime, chl_ice$chl, rule = 2)(km$start_time)
 
-# acoustic data
-acoustic <- readRDS("./derived data/midoc_acoustic_sums.rda")
+km <- km %>% select(-bestley.zone, -DNC.auto)
 
-# chlorophyll and ice
-chl_ice <- read_rds("./derived data/k-axis-midoc-satdata-extraction_2017-11-09.rds")
+# in-situ integrated chlorophyll from Karen Westwood
+intchl <- readxl::read_xlsx("./source data/Final Integrated Chlorophyll.xlsx", skip = 4)
+colnames(intchl) <- c("CTD.n", "lat", "lon", "no", "intChl", "comments")
 
-## TODO: going forward; locations of stations 4 and 5 are a bit off...
-ggplot(km, aes(x=lon_start, y=lat_start)) + geom_point() + geom_path(data=nav, aes(x=LONGITUDE, y=LATITUDE)) +geom_point(data=chl_ice, aes(x=LONGITUDE,y=LATITUDE), col="blue", size=3) + geom_text(data=chl_ice, aes(label=substr(midoc.stn, 6,8), x=LONGITUDE, y=LATITUDE))
+# CTD station locations from this file were visually matched to closest midoc stations using the following plot; with the matches recorded in midoc_CTD_matching.xlsx
+# ggplot(nav, aes(y=LATITUDE, x=LONGITUDE)) +geom_path() + geom_point(data=km, aes(x=lon_start, y=lat_start), col="blue") + geom_point(data=km, aes(x=lon_end, y=lat_end), col="red") +geom_point(data=intchl, aes(x=lon, y=-lat), col="green") + geom_text(data=km, aes(x=lon_end, y=lat_end, label=substr(midoc.stn, 6,8)), col="red", hjust=0) + geom_text(data=intchl, aes(x=lon, y=-lat, label=CTD.n), col="dark green", hjust=1.2)
 
-# biomass of taxa:
-bm.tax <- readRDS("./derived data/codend_taxa_biomass.rds")
-bm.tax <- acoustic %>% ungroup() %>% select(sum_NASC, midoc.stn, cod.end) %>% inner_join(bm.tax) %>% 
-	inner_join(stn)
-bm.tax <- chl_ice %>% select(midoc.stn, days_since_melt, distance_to_ice_m, distance_to_edge_m, sea_ice_conc, chl) %>% inner_join(bm.tax)
-bm.tax<- dnc %>% select(midoc.stn, DNC.visual) %>% inner_join(bm.tax)
+ctd.midoc <- readxl::read_xlsx("./source data/midoc_CTD_matching.xlsx", skip=1)
+intchl$midoc <- ctd.midoc$MIDOC[match(intchl$no, ctd.midoc$CTD)]
 
-## TODO: check that the filter is actually working for station!!!!!
+km$intChl <- intchl$intChl[match(km$midoc.stn, paste0("MIDOC",intchl$midoc))]
 
-# biomass plots
-	# first cut with ggplot
-	# total biomass
-	p.d<- bm.tax %>% filter(cod.end%in%"front.of.net"==F & midoc.stn%in%c("TRIAL","MIDOC02","MIDOC08","MIDOC10","MIDOC12","MIDOC13","MIDOC3")==F) %>%
-	filter(tax.grp%in%"NA"==F)
-	ggplot(data=p.d, aes(x=cod.end, weight=bm, fill=tax.grp, facets=midoc.stn), geom="bar", ylab="biomass") +
-	geom_bar() +
-	theme_bw() +
-	coord_flip() +
-	facet_wrap(~midoc.stn) +
-	theme(axis.text.x = element_text(angle = 90, hjust = 1))
-	# ggsave("biomass_by_coarse_taxonomic_grp190216.pdf")
-
-	# proportions
-	ggplot(data=p.d, aes(x=cod.end, weight=pbm, fill=tax.grp, facets=midoc.stn), geom="bar", ylab="% biomass") +
-	geom_bar() +
-	theme_bw() +
-	facet_wrap(~midoc.stn) +
-	coord_flip() +
-	theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-	#ggsave("proportion_biomass_by_coarse_taxonomic_grp190216.pdf")
-
-
-# NASC vs. biomass
-ggplot(p.d %>% filter(tax.grp %in% c("cephalopods","cnidarians","fish","krill","salps")), 
-	aes(y=bm, x=sum_NASC, color=DNC.visual)) +
-	geom_point() +
-	facet_grid(cod.end ~ tax.grp) +
-	scale_x_log10() + scale_y_log10() +
-	geom_smooth(method='lm', aes(color=NA)) +
-	theme_bw()
-
-# Biomass vs. chlorophyll
-ggplot(p.d %>% filter(tax.grp %in% c("cephalopods","cnidarians","fish","krill","salps"),bm<1e5), 
-	aes(y=bm, x=chl, colour=DNC.visual)) +
-	geom_point() +
-	facet_grid(cod.end ~ tax.grp) +
-#	scale_x_log10() + scale_y_log10() +
-	geom_smooth(method='lm',aes(color=NA)) +
-	theme_bw()
-
-## Pairs plots!
-	bm.wide <- bm.tax %>% spread(key=tax.grp, value=bm, fill=0)
-	library(GGally)
-	
-	# different taxa
-	bm.wide %>% select(fish, cnidarians, krill, salps, cephalopods) %>% ggpairs() +
-	## not very informative 
-
-	# for each taxon, a splom of env predictors vs. biomass
-	bm.wide %>% select(days_since_melt, distance_to_ice_m, chl, Tmin_depth, DNC.visual, fish) %>% ggpairs() 
-	bm.wide %>% select(days_since_melt, distance_to_ice_m, chl, Tmin_depth, cnidarians) %>% ggpairs() 
-	bm.wide %>% select(days_since_melt, distance_to_ice_m, chl, Tmin_depth, krill) %>% ggpairs() 
-	bm.wide %>% select(days_since_melt, distance_to_ice_m, chl, Tmin_depth, salps) %>% ggpairs() 
-	bm.wide %>% select(days_since_melt, distance_to_ice_m, chl, Tmin_depth, cephalopods) %>% ggpairs() 
-
-	# can add colour to select and ., mapping=ggplot2::aes(colour = cod.end) to plot statement but ugly
-## fish! models
-
-
-# biomass of fish groups:
-bm.fish <- readRDS("./derived data/codend_fish_biomass.rds")
-bm.fish <- acoustic %>% ungroup() %>% select(sum_NASC, midoc.stn, cod.end) %>% inner_join(bm.fish)
-bm.fish <- bm.fish %>% inner_join(stn)
-
-
-#library(lme4)
-libraruy(MuMIn)
-
-
-env.dat <- bm.wide %>% select(midoc.stn, DNC.visual, chl, distance_to_edge_m, days_since_melt, LONGITUDE, LATITUDE, Tmin_depth) %>% distinct(midoc.stn, .keep_all = T)
-env.nasc <- bm.tax %>% filter(tax.grp%in%c("fish","cephalopods","cnidarians","krill","salps")) %>%
-	ungroup() %>%
-	group_by(midoc.stn) %>%
-	summarise(sum_NASC=sum(sum_NASC, na.rm=T)) %>%
-	inner_join(env.dat)
-
-mod.dat <- bm.tax %>% filter(cod.end%in%"1"==F, tax.grp%in%c("fish","cephalopods","cnidarians","krill","salps")) %>%group_by(midoc.stn, tax.grp) %>% summarise(bm.tax = sum(bm)) %>% spread(key=tax.grp, value=bm.tax, fill=0) %>% inner_join(env.nasc) 
-
-options(na.action = "na.fail")
-library(MuMIn)
-library(coefplot2)
-
-# nasc vs. biomass of taxa, full water column
-m1.sat <- lm(log(sum_NASC) ~ log(fish+1) + log(cephalopods+1) + log(cnidarians+1) + log(krill+1) + log(salps+1)  , data=mod.dat)
-m1.d <- dredge(m1.sat)
-m1.d
-m1.avg<- model.avg(subset(m1.d, delta<=2))
-coefplot(m1.sat)
-
-# version of this for only CE5 & 6
-mod.dat56 <- bm.tax %>% filter(cod.end%in%"1"==F, tax.grp%in%c("fish","cephalopods","cnidarians","krill","salps", cod.end%in%c("5",6))) %>%group_by(midoc.stn, tax.grp) %>% summarise(bm.tax = sum(bm)) %>% spread(key=tax.grp, value=bm.tax, fill=0) %>% inner_join(env.nasc)
-
-m1a.sat <- lm(log(sum_NASC) ~ log(fish+1) + log(cephalopods+1) + log(cnidarians+1) + log(krill+1) + log(salps+1)  , data=mod.dat56)
-m1.d <- dredge(m1.sat)
-m1.d
-m1.avg<- model.avg(subset(m1.d, delta<=2))
-
-
-
-# fish biomass vs env predictors
-m2.sat <- lm(log(fish+1) ~ DNC.visual + chl + distance_to_edge_m + days_since_melt + LONGITUDE + LATITUDE + Tmin_depth, data=mod.dat)
-m2.d <- dredge(m1.sat)
-m2.d
-m2.avg<- model.avg(subset(m1.d, delta<=2))
-
-# same as M2, non log
-m3.sat <- lm(fish ~ DNC.visual + chl + distance_to_edge_m + days_since_melt + LONGITUDE + LATITUDE + Tmin_depth, data=mod.dat)
-coefplot2(m3.sat)
-coefplot2(m2.sat)
-
-# standardised predictors
-mod.dat2 <- mod.dat %>% select(fish, DNC.visual, chl, distance_to_edge_m, days_since_melt, LONGITUDE, LATITUDE, Tmin_depth)
-
-library(mgcv)
-gam1<- gam(fish ~ + s(chl) + s(distance_to_edge_m) + s(Tmin_depth) + s(LATITUDE), data=mod.dat)
-
-
-mod.dat.CE <- bm.tax %>% filter(cod.end%in%"1"==F, tax.grp%in%c("fish","cephalopods","cnidarians","krill","salps")) %>%group_by(midoc.stn, tax.grp, cod.end) %>% summarise(bm.tax = sum(bm)) %>% spread(key=tax.grp, value=bm.tax, fill=0) %>% inner_join(env.nasc) 
-
-m4.sat <- lm(log(fish +1) ~ DNC.visual*cod.end + chl + distance_to_edge_m + days_since_melt + LONGITUDE + LATITUDE + Tmin_depth, data=mod.dat.CE)
-
-	coefplot2(m4.sat) ## a lot going on here... would need to do partial dependency plots to unpack it=
-
-mod.dat.CE$depth <- ifelse(mod.dat.CE$cod.end%in%"2", 900, ifelse(mod.dat.CE$cod.end%in%"3", 700, ifelse(mod.dat.CE$cod.end%in%"4", 500, ifelse(mod.dat.CE$cod.end%in%"5", 300,100))))
-
-m5.sat <- lm(log(fish +1) ~ DNC.visual*depth + chl + distance_to_edge_m + days_since_melt + LONGITUDE + LATITUDE + Tmin_depth, data=mod.dat.CE)
-m5.avg<- model.avg(subset(m5.d, delta<=2))
-
-m5.pd <- cbind(data.frame(coef=coef(m5.avg)), confint(m5.avg))
-
-par(mar=c(2,10,2,1), las=1)
-plot(c(-10,10), c(0, nrow(m5.pd)), axes=F, type="n", ylab="")
-axis(1)
-axis(2, at=1:nrow(m5.pd), labels = row.names(m5.pd))
-abline(v=0, lty="dashed")
-points(m5.pd$coef, 1:nrow(m5.pd), pch=19)
-segments(m5.pd[,2], 1:nrow(m5.pd), m5.pd[,3],1:nrow(m5.pd))
+saveRDS(km, "./derived data/midoc_stations_envdata.rda")
+readr::write_csv(km, "./derived data/midoc_stations_envdata.csv")
 
 
--- README.md
+++ README.md
@@ -4,15 +4,16 @@ Repository for data, data processing scripts and analysis scripts for midwater t
 
 ## Workflow
 ### Data processing and summary scripts
-- '01_import_net_logger_and_voyage_track.R'
-- '02_import_catch.R'
-- '03_shot_summary_stats.R'
-- '04_md_acoustic.R'
+- '01_import_net_logger_and_voyage_track.R' imports data from midoc on-board logger (including CTD and records of when nets triggered) and from Aurora Australis nav files and extracts start and end locations and times for each shot and cod-end, calculates volumes swept, and assigns crepuscular category.
+- '02_import_catch.R': imports 'sample data' and 'codend data' and generates summaries of biomass of taxonomic groups caught per cod-end
+- '03_shot_summary_stats.R': calculates basic summary info like average speed, distances covered and volumes swept for tows
+- '04_md_acoustic.R': calculates integrated NASC (acoustic signal) for each midoc cod-end
 - '05_catch_env.R'
 
 ### Scripts specific to Kerguelen Plateau Symposium Extended Abstract
 - '/KPS_symposium_extended_abstract/KPS_EA_fig1_map.R'
 - '/KPS_symposium_extended_abstract/KPS_EA_catch_bubbles.R'
+- '/KPS_symposium_extended_abstract/KPS_EA_models.R'
 
 ### Scripts specific to paper for Deep Sea Research Special Issue
 
