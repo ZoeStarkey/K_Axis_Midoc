944f172d9912ea3bf7b234fac2e850cb9a0ba7dc
RTreb <rowan.trebilco@utas.edu.au>
2018-09-07 12:14:19 +1000

    more dirty hippies

--- fish_habitat_modelling/02_get_HIPPIES_fish_data.R
+++ fish_habitat_modelling/02_get_HIPPIES_fish_data.R
@@ -2,3 +2,14 @@
 # script to bring in HIPPIES fish data, and to convert to format appropriate for habitat modelling
 
 library(tidyverse)
+usr <- Sys.info()["user"]
+d<- paste0("/Users/", usr, "/GitHub/K_axis_midoc/fish_habitat_modelling")
+setwd(d)
+
+# fish table
+hfd <- read_csv("../source data/HIPPIES DB/Fish Data.csv")
+
+# haul table
+hh <- read_csv("../source data/HIPPIES DB/Fish Data.csv")
+
+#
