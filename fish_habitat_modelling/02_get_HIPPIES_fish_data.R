529c3d6711bf30be62239441c1086c6b0b23f534
RTreb <rowan.trebilco@utas.edu.au>
2018-09-14 14:32:51 +1000

    turning the crank on HIPPIES

--- fish_habitat_modelling/02_get_HIPPIES_fish_data.R
+++ fish_habitat_modelling/02_get_HIPPIES_fish_data.R
@@ -2,6 +2,7 @@
 # script to bring in HIPPIES fish data, and to convert to format appropriate for habitat modelling
 
 library(tidyverse)
+library(readxl)
 usr <- Sys.info()["user"]
 d<- paste0("/Users/", usr, "/GitHub/K_axis_midoc/fish_habitat_modelling")
 setwd(d)
@@ -13,3 +14,11 @@ hfd <- read_csv("../source data/HIPPIES DB/Fish Data.csv")
 hh <- read_csv("../source data/HIPPIES DB/Fish Data.csv")
 
 # species list table... check if this aligns properly
+sp <- read_xlsx("../source data/HIPPIES DB/SpeciesList.xlsx")
+
+# restrict to gear type 35 == IYGPT
+
+# relevant columns of fish data
+hfd <- hfd %>% select("Index","Serial Number","HaulID","Fish Type","Weight (g)")
+
+# match to relevant bits of 
