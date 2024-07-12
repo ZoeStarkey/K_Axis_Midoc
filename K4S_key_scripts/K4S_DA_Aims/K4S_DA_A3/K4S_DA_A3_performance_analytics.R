53924a8d96cbcf1b4b3c73087c1d7e067b880304
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-07-12 12:24:19 +1000

    K4S_DA_A3_Performance_Analytics
    Creating a new script to investigate performance analytics for environmental variables

---
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_performance_analytics.R
@@ -0,0 +1,20 @@
+library(PerformanceAnalytics)
+library(ggplot2)
+library(mgcv)
+library(caret)
+
+
+#Convert to numeric 
+# km_sf$CHLA <- as.numeric(km_sf$CHLA)
+# km_sf$TSM <- as.numeric(km_sf$TSM)
+# km_sf$CUR <- as.numeric(km_sf$CUR)
+
+env_vars <- km_df[, c("CHLA", "TSM", "CUR", "SST", "Tmin", "SML", "lunar_fraction")]
+env_vars <- env_vars[complete.cases(env_vars), ]
+
+
+
+
+
+
+chart.Correlation(env_vars, histogram=TRUE, pch=19)
