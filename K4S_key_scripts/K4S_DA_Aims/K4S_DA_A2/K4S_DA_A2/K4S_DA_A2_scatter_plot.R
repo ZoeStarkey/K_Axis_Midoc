7a85afc81dacf2e5b9e64004db51333d74437bf7
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-07-19 18:07:59 +1000

    K4S_DA_A2_Scatter_Plot: Plotting smooth
    Added a smooth function to both the total taxa - gelatinous and to the fish and squidd plots.
    
    Also made the dataframe an RDA to make it easier to call into the environment

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A2/K4S_DA_A2/K4S_DA_A2_scatter_plot.R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A2/K4S_DA_A2/K4S_DA_A2_scatter_plot.R
@@ -87,21 +87,10 @@ q2 <- 10
 R[R > q2] <- q2
 R[R < q1] <- q1
 R <- log(R)
-zz <- c(0.05, 0.1, 0.25, 0.5, 1, 2.5, 5,7.5)
-log_zz <- log(zz)
+#zz <- c(0.05, 0.1, 0.25, 0.5, 1, 2.5, 5,7.5)
+#log_zz <- log(zz)
 
-# Convert raster to data frame
-R_df <- as.data.frame(R, xy = TRUE)
-colnames(R_df) <- c("x", "y", "value")
-
-# Check CRS of the raster and km_sf
-raster_crs <- crs(R)
-km_sf_crs <- st_crs(km_sf)
-
-# If they do not match, transform the CRS of km_sf
-if (km_sf_crs != raster_crs) {
-  km_sf <- st_transform(km_sf, crs = raster_crs)
-}
+km_sf <- st_transform(km_sf, crs = crs(R))
 
 # Initialize a column to store CHLA values
 km_df$CHLA <- NA
@@ -243,81 +232,8 @@ abandoned_stations <- c("MIDOC02","MIDOC08", "MIDOC10", "MIDOC12", "MIDOC33")
 km_df <- km_df %>%
   filter(!midoc.stn %in% abandoned_stations)
 
-#PLOTTING
-TBM_scatter_aggregate <- function(data, x_var, y_var = "bm_g_m3", depth_var = "depth", aggregate_func = c("sum", "mean"), x_label = NULL, y_label = NULL, title = NULL) {
-  # Exclude specified taxa
-  exclude_taxa <- c("cnidarians", "salps", "mixed/other gelatinous", "mixed krill and salps")
-  data_filtered <- data[!data$tax.grp %in% exclude_taxa, ]
-  
-  # Aggregate data by depth bins
-  data_aggregated <- data_filtered %>%
-    group_by_at(vars(depth_var, x_var)) %>%
-    summarize(
-      total_biomass = sum(!!sym(y_var), na.rm = TRUE),
-      mean_biomass = mean(!!sym(y_var), na.rm = TRUE)
-    ) %>%
-    ungroup()
-  
-  # Define labels if not provided
-  if (is.null(x_label)) x_label <- x_var
-  if (is.null(y_label)) y_label <- expression(paste("Biomass g m"^"-3"))
-  if (is.null(title)) title <- paste("Scatterplot of Taxa Biomass (excluding gelatinous) vs", x_var)
-  
-  plots <- list()
-  
-  # Create scatter plots for each aggregation type
-  for (func in aggregate_func) {
-    if (func == "sum") {
-      p <- ggplot(data_aggregated, aes_string(x = x_var, y = "total_biomass", color = depth_var, group = depth_var)) +
-        geom_point(size = 5) +  # Add trend lines
-        labs(title = paste(title, "(Sum)"),
-             x = x_label,
-             y = y_label,
-             color = "Depth Bin") +
-        theme_minimal() +
-        scale_color_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue", "0-1000m" = "white"))
-      plots[["sum"]] <- p
-    } else if (func == "mean") {
-      p <- ggplot(data_aggregated, aes_string(x = x_var, y = "mean_biomass", color = depth_var, group = depth_var)) +
-        geom_point(size = 5) +  # Add trend lines
-        labs(title = paste(title, "(Mean)"),
-             x = x_label,
-             y = y_label,
-             color = "Depth Bin") +
-        theme_minimal() +
-        scale_color_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue", "0-1000m" = "white"))
-      plots[["mean"]] <- p
-    }
-  }
-  
-  return(plots)
-}
-
-# Example usage
-# Define the depth bins
-depth_bins <- c("0-1000m", "800-1000m", "600-800m", "400-600m", "200-400m", "0-200m")
-
-km_df$depth <- factor(km_df$cod.end, levels = c("1", "2", "3", "4", "5", "6"), labels = depth_bins)
-
-# Create scatter plots for summed and mean biomass
-plot <- TBM_scatter_aggregate(km_df, "CHLA")
-plot <- TBM_scatter_aggregate(km_df, "SST")
-plot <- TBM_scatter_aggregate(km_df, "TSM")
-plot <- TBM_scatter_aggregate(km_df, "CUR")
-plot <- TBM_scatter_aggregate(km_df, "Tmin")
-plot <- TBM_scatter_aggregate(km_df, "O2_min")
-plot <- TBM_scatter_aggregate(km_df, "SML")
-plot <- TBM_scatter_aggregate(km_df, "lunar_fraction")
-plot <- TBM_scatter_aggregate(km_df, "moon_phase")
-plot <- TBM_scatter_aggregate(km_df, "altitude")
-
-# Print the plots
-print(plot[["sum"]])
-print(plot[["mean"]])
-
-
-
-
+#saving the data_frame 
+save(km_df, file = "km_df_environmental_variables.Rda")
 
 #PLOTTING
 TBM_scatter_aggregate <- function(data, x_var, y_var = "bm_g_m3", depth_var = "depth", aggregate_func = c("sum", "mean"), x_label = NULL, y_label = NULL, title = NULL) {
@@ -445,7 +361,6 @@ TBM_scatter_aggregate <- function(data, x_var, y_var = "bm_g_m3", depth_var = "d
   return(plots)
 }
 
-# Example usage
 # Define the depth bins
 depth_bins <- c("0-1000m", "800-1000m", "600-800m", "400-600m", "200-400m", "0-200m")
 
@@ -466,10 +381,6 @@ plot <- TBM_scatter_aggregate(km_df, "altitude")
 # Print the plots
 print(plot[["sum"]])
 print(plot[["mean"]])
-
-
-
-
 
 #PLOTTING - FISH/CEPHALOPODS
 TBM_scatter_aggregate <- function(data, x_var, y_var = "bm_g_m3", depth_var = "depth", aggregate_func = c("sum", "mean"), x_label = NULL, y_label = NULL, title = NULL) {
@@ -555,12 +466,3 @@ print(plot[["mean"]])
 
 
 
-
-
-# Define the taxa groups to exclude
-excluded_taxa <- c("cnidarians", "cephalopods", "fish", "salps", "krill")
-
-# Create the new dataframe
-new_df <- km_df %>%
-  filter(!tax.grp %in% excluded_taxa) %>%
-  select(midoc.stn, tax.grp)
