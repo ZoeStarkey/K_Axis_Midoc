34f33d5deed2c1f1fb5cc2b764f92a767387112c
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-07-23 11:11:00 +1000

    K4S_DA_A3_box_whisker: plotting adjustment + moving bits of code around.
    Moved my initial attemps of code to the bottom of the page. Added in Smax

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_box_whisker_plots.R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_box_whisker_plots.R
@@ -22,107 +22,6 @@ include_taxa <- c("fish")
 km_df_filtered <- km_df_filtered[km_df_filtered$tax.grp %in% include_taxa, ]
 
 
-
-
-
-#TSM - unbinned 
-# Remove rows with NA values in TSM
-km_df_filtered <- km_df_filtered %>%
-  filter(!is.na(TSM))
-
-# Plot the data with formatted x-axis labels
-ggplot(km_df_filtered, aes(x = as.factor(TSM), y = bm_g_m3, fill = depth)) +
-  geom_boxplot() +
-  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
-  theme_bw() +
-  xlab("TSM") +
-  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
-  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
-  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
-  guides(fill = guide_legend(reverse = TRUE)) +
-  scale_x_discrete(labels = function(x) sprintf("%.1f", as.numeric(as.character(x)))) +
-  theme(
-    strip.text.y = element_text(angle = 0),
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
-  )
-
-
-#zoomed in 
-
-ggplot(km_df_filtered, aes(x = as.factor(TSM), y = bm_g_m3, fill = depth)) +
-  geom_boxplot() +
-  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
-  theme_bw() +
-  xlab("TSM") +
-  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
-  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
-  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
-  guides(fill = guide_legend(reverse = TRUE)) +
-  scale_x_discrete(labels = function(x) sprintf("%.1f", as.numeric(as.character(x)))) +
-  coord_cartesian(ylim = c(0, 0.02)) +  # Adjust the y-axis range as needed
-  theme(
-    strip.text.y = element_text(angle = 0),
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
-  )
-
-#TSM - Binned 
-
-# Define the number of bins
-num_bins <- 5
-
-# Calculate the breaks for TSM
-breaks <- seq(min(km_df_filtered$TSM, na.rm = TRUE), max(km_df_filtered$TSM, na.rm = TRUE), length.out = num_bins + 1)
-
-# Ensure the breaks are unique by adding a small epsilon if necessary
-epsilon <- 1e-6
-breaks <- unique(c(breaks, breaks[length(breaks)] + epsilon))
-
-# Create bins for TSM
-km_df_filtered <- km_df_filtered %>%
-  mutate(TSM_binned = cut(TSM, breaks = breaks, include.lowest = TRUE, labels = paste(head(breaks, -1), tail(breaks, -1) - epsilon, sep = " - ")))
-
-# Create bins for TSM with formatted labels
-km_df_filtered <- km_df_filtered %>%
-  mutate(TSM_binned = cut(TSM, breaks = breaks, include.lowest = TRUE, 
-                          labels = paste(sprintf("%.2f", head(breaks, -1)), sprintf("%.2f", tail(breaks, -1)), sep = " - ")))
-
-
-
-ggplot(km_df_filtered, aes(x = TSM_binned, y = bm_g_m3, fill = depth)) +
-  geom_boxplot() +
-  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
-  theme_bw() +
-  xlab("TSM") +
-  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
-  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
-  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
-  guides(fill = guide_legend(reverse = TRUE)) +
-  theme(
-    strip.text.y = element_text(angle = 0),
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
-  )
-
-
-#Trying to zoom in 
-ggplot(km_df_filtered, aes(x = TSM_binned, y = bm_g_m3, fill = depth)) +
-  geom_boxplot(outlier.shape = NA) +
-  #geom_jitter(width = 0.2, alpha = 0.2) +  # Add jitter for better visibility of points
-  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
-  theme_bw() +
-  xlab("TSM") +
-  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
-  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
-  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
-  guides(fill = guide_legend(reverse = TRUE)) +
-  coord_cartesian(ylim = c(0, 0.01)) +  # Adjust the y-axis range as needed
-
-  theme(
-    strip.text.y = element_text(angle = 0),
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
-  )
-
-
-
 #BINNED FUNCTION 
 # Define the function
 create_boxplot <- function(data, env_var, dep_var, num_bins = 5, depth_col = "depth") {
@@ -179,6 +78,7 @@ plot_sst <- create_boxplot(km_df_filtered, env_var = "SST", dep_var = "bm_g_m3")
 plot_tmin <- create_boxplot(km_df_filtered, env_var = "Tmin", dep_var = "bm_g_m3")
 plot_o2_min <- create_boxplot(km_df_filtered, env_var = "O2_min", dep_var = "bm_g_m3")
 plot_sml <- create_boxplot(km_df_filtered, env_var = "SML", dep_var = "bm_g_m3")
+plot_Smax <- create_boxplot(km_df_filtered, env_var = "Smax", dep_var = "bm_g_m3")
 plot_lunar_fraction <- create_boxplot(km_df_filtered, env_var = "lunar_fraction", dep_var = "bm_g_m3")
 plot_moon_phase <- create_boxplot(km_df_filtered, env_var = "moon_phase", dep_var = "bm_g_m3")
 plot_altitude <- create_boxplot(km_df_filtered, env_var = "altitude", dep_var = "bm_g_m3")
@@ -191,6 +91,7 @@ save_plot(plot_sst, "boxplot_sst_no_gelat.png")
 save_plot(plot_tmin, "boxplot_tmin_no_gelat.png")
 save_plot(plot_o2_min, "boxplot_o2_min_no_gelat.png")
 save_plot(plot_sml, "boxplot_sml_no_gelat.png")
+save_plot(plot_Smax, "boxplot_Smax_no_gelat.png")
 save_plot(plot_lunar_fraction, "boxplot_lunar_fraction_no_gelat.png")
 save_plot(plot_moon_phase, "boxplot_moon_phase_no_gelat.png")
 save_plot(plot_altitude, "boxplot_altitude_no_gelat.png")
@@ -284,16 +185,17 @@ create_boxplot <- function(data, env_var, dep_var, num_bins = 5, depth_col = "de
 # Example usage
 # Assuming km_df_filtered is your dataframe and you want to plot for TSM and bm_g_m3
 # and zoom in with y-axis limits between 0 and 0.01
-plot_tsm <- create_boxplot(km_df_filtered, env_var = "TSM", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_chla <- create_boxplot(km_df_filtered, env_var = "CHLA", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_cur <- create_boxplot(km_df_filtered, env_var = "CUR", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_sst <- create_boxplot(km_df_filtered, env_var = "SST", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_tmin <- create_boxplot(km_df_filtered, env_var = "Tmin", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_o2_min <- create_boxplot(km_df_filtered, env_var = "O2_min", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_sml <- create_boxplot(km_df_filtered, env_var = "SML", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_lunar_fraction <- create_boxplot(km_df_filtered, env_var = "lunar_fraction", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_moon_phase <- create_boxplot(km_df_filtered, env_var = "moon_phase", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
-plot_altitude <- create_boxplot(km_df_filtered, env_var = "altitude", dep_var = "bm_g_m3", y_limits = c(0, 0.001))
+plot_tsm <- create_boxplot(km_df_filtered, env_var = "TSM", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_chla <- create_boxplot(km_df_filtered, env_var = "CHLA", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_cur <- create_boxplot(km_df_filtered, env_var = "CUR", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_sst <- create_boxplot(km_df_filtered, env_var = "SST", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_tmin <- create_boxplot(km_df_filtered, env_var = "Tmin", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_o2_min <- create_boxplot(km_df_filtered, env_var = "O2_min", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_sml <- create_boxplot(km_df_filtered, env_var = "SML", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_Smax <- create_boxplot(km_df_filtered, env_var = "Smax", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_lunar_fraction <- create_boxplot(km_df_filtered, env_var = "lunar_fraction", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_moon_phase <- create_boxplot(km_df_filtered, env_var = "moon_phase", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
+plot_altitude <- create_boxplot(km_df_filtered, env_var = "altitude", dep_var = "bm_g_m3", y_limits = c(0, 0.003))
 
 
 
@@ -305,6 +207,7 @@ save_plot(plot_sst, "boxplot_sst_no_gelat.png")
 save_plot(plot_tmin, "boxplot_tmin_no_gelat.png")
 save_plot(plot_o2_min, "boxplot_o2_min_no_gelat.png")
 save_plot(plot_sml, "boxplot_sml_no_gelat.png")
+save_plot(plot_Smax, "boxplot_Smax_no_gelat.png")
 save_plot(plot_lunar_fraction, "boxplot_lunar_fraction_no_gelat.png")
 save_plot(plot_moon_phase, "boxplot_moon_phase_no_gelat.png")
 save_plot(plot_altitude, "boxplot_altitude_no_gelat.png")
@@ -321,3 +224,99 @@ save_plot(plot_lunar_fraction, "boxplot_lunar_fraction_ceph.png")
 save_plot(plot_moon_phase, "boxplot_moon_phase_ceph.png")
 save_plot(plot_altitude, "boxplot_altitude_ceph.png")
 
+
+#TSM - unbinned 
+# Remove rows with NA values in TSM
+km_df_filtered <- km_df_filtered %>%
+  filter(!is.na(TSM))
+
+# Plot the data with formatted x-axis labels
+ggplot(km_df_filtered, aes(x = as.factor(TSM), y = bm_g_m3, fill = depth)) +
+  geom_boxplot() +
+  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
+  theme_bw() +
+  xlab("TSM") +
+  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
+  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
+  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  scale_x_discrete(labels = function(x) sprintf("%.1f", as.numeric(as.character(x)))) +
+  theme(
+    strip.text.y = element_text(angle = 0),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+  )
+
+
+#zoomed in 
+
+ggplot(km_df_filtered, aes(x = as.factor(TSM), y = bm_g_m3, fill = depth)) +
+  geom_boxplot() +
+  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
+  theme_bw() +
+  xlab("TSM") +
+  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
+  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
+  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  scale_x_discrete(labels = function(x) sprintf("%.1f", as.numeric(as.character(x)))) +
+  coord_cartesian(ylim = c(0, 0.02)) +  # Adjust the y-axis range as needed
+  theme(
+    strip.text.y = element_text(angle = 0),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+  )
+
+#TSM - Binned 
+
+# Define the number of bins
+num_bins <- 5
+
+# Calculate the breaks for TSM
+breaks <- seq(min(km_df_filtered$TSM, na.rm = TRUE), max(km_df_filtered$TSM, na.rm = TRUE), length.out = num_bins + 1)
+
+# Ensure the breaks are unique by adding a small epsilon if necessary
+epsilon <- 1e-6
+breaks <- unique(c(breaks, breaks[length(breaks)] + epsilon))
+
+# Create bins for TSM
+km_df_filtered <- km_df_filtered %>%
+  mutate(TSM_binned = cut(TSM, breaks = breaks, include.lowest = TRUE, labels = paste(head(breaks, -1), tail(breaks, -1) - epsilon, sep = " - ")))
+
+# Create bins for TSM with formatted labels
+km_df_filtered <- km_df_filtered %>%
+  mutate(TSM_binned = cut(TSM, breaks = breaks, include.lowest = TRUE, 
+                          labels = paste(sprintf("%.2f", head(breaks, -1)), sprintf("%.2f", tail(breaks, -1)), sep = " - ")))
+
+
+
+ggplot(km_df_filtered, aes(x = TSM_binned, y = bm_g_m3, fill = depth)) +
+  geom_boxplot() +
+  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
+  theme_bw() +
+  xlab("TSM") +
+  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
+  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
+  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  theme(
+    strip.text.y = element_text(angle = 0),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+  )
+
+
+#Trying to zoom in 
+ggplot(km_df_filtered, aes(x = TSM_binned, y = bm_g_m3, fill = depth)) +
+  geom_boxplot(outlier.shape = NA) +
+  #geom_jitter(width = 0.2, alpha = 0.2) +  # Add jitter for better visibility of points
+  facet_wrap(~ reorder(depth, desc(depth)), ncol = 1, scales = "fixed") +
+  theme_bw() +
+  xlab("TSM") +
+  ylab(expression(paste("Biomass (g m"^"-3", ")"))) +
+  ggtitle("Boxplot of Biomass excluding gelatinous by Depth Categories and TSM") +
+  scale_fill_manual(values = c("0-200m" = "#FFD300", "200-400m" = "red", "400-600m" = "magenta", "600-800m" = "purple", "800-1000m" = "blue")) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  coord_cartesian(ylim = c(0, 0.01)) +  # Adjust the y-axis range as needed
+  
+  theme(
+    strip.text.y = element_text(angle = 0),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)
+  )
