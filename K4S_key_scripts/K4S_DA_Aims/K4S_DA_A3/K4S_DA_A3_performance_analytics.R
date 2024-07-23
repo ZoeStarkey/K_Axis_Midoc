62afbd34f1d1739f92f8abb270a8acaa187429e4
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-07-23 12:48:48 +1000

    K4S_DA_A3_performance_analytics: Adding plot Added a plot for fish and squid

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_performance_analytics.R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_performance_analytics.R
@@ -19,6 +19,12 @@ km_df_filtered <-  km_df_filtered[!km_df_filtered$tax.grp %in% exclude_taxa, ]
 env_vars <- km_df_filtered[, c("TSM", "CUR", "SST","CHLA", "Tmin", "Tmax", "O2_min", "SML", "Smax", "lunar_fraction", "moon_phase", "altitude")]
 env_vars <- env_vars[complete.cases(env_vars), ]
 
+#just fish and swuid 
 
+include_taxa <- c("cephalopods", "fish")
+km_df_filtered <- km_df_filtered[km_df_filtered$tax.grp %in% include_taxa, ]
+
+env_vars <- km_df_filtered[, c("TSM", "CUR", "SST","CHLA", "Tmin", "Tmax", "O2_min", "SML", "Smax", "lunar_fraction", "moon_phase", "altitude")]
+env_vars <- env_vars[complete.cases(env_vars), ]
 
 chart.Correlation(env_vars, histogram=TRUE, pch=19)
