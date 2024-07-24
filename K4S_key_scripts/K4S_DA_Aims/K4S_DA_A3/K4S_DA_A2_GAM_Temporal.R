991f10daa8cca9231707b5c5068ffe639deb62c0
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-07-24 17:16:36 +1000

    K4S_DA_A2_GAM_Temporal: altering dataframe Made start_time into a numeric value for modelling, put it in a new column called day

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A2_GAM_Temporal.R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A2_GAM_Temporal.R
@@ -1,6 +1,7 @@
 #library
 library(mgcv)
 library(gratia)
+library(dplyr)
 
 #linear model
 usr <- Sys.info()["user"]
@@ -26,13 +27,23 @@ km_df_total <- km_df %>%
   )
 
 
+# Adding day 
+km_df_total <- km_df_total %>%
+  mutate(day = as.numeric(as.POSIXct(start_time)) / (60 * 60 * 24))
+
+#
+m1 <- gam(log(total_biomass) ~ s(start_time),data = km_df_total)
+
+
 #Lunar fraction 
-m1 <- gam(log(total_biomass) ~ s(lunar_fraction, by = depth),data = km_df_total)
-draw(m1, residuals = TRUE) 
-
-
+m2 <- gam(log(total_biomass) ~ s(lunar_fraction),data = km_df_total)
+draw(m2, residuals = TRUE) 
 summary(m1)
 
 
+
+
+new_df <- km_df_total %>%
+  select(midoc.stn, start_time, day)
 
 
