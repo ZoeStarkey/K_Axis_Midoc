9aceaef8b8c57004ddf1dbe89f166a6f7960614e
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-07-30 10:40:53 +1000

    K4S_DA_A3_performance_analytics: Adding to dataframe
    Added day to dataframe
    Logged biomass
    Started to split by gelatinous

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_performance_analytics.R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A3/K4S_DA_A3_performance_analytics.R
@@ -6,6 +6,22 @@ library(caret)
 
 \
 #All Taxa
+usr <- Sys.info()["user"]
+d<- paste0("/Users/", usr, "/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts")
+setwd(d)
+dir.exists(d)
+
+#Summed data - one biom data point per station 
+load("km_df_environmental_variables.Rda")
+
+#add day 
+km_df <- km_df %>%
+  mutate(day = as.numeric(as.POSIXct(start_time)) / (60 * 60 * 24))
+
+#add log
+km_df$logged_bm <-log(km_df$bm_g_m3)
+
+
 env_vars <- km_df[, c("TSM", "CUR", "SST","CHLA", "Tmin", "Tmax", "O2_min", "SML", "Smax", "lunar_fraction", "moon_phase", "altitude")]
 env_vars <- env_vars[complete.cases(env_vars), ]
 
@@ -15,6 +31,15 @@ km_df_filtered <- km_df %>%
   filter(!is.na(depth) & depth != "0-1000m")
 exclude_taxa <- c("cnidarians", "salps", "mixed/other gelatinous", "mixed krill and salps")
 km_df_filtered <-  km_df_filtered[!km_df_filtered$tax.grp %in% exclude_taxa, ]
+
+
+
+#Temporal 
+
+temporal_variables <- km_df_filtered[,c("logged_bm", "lunar_fraction","altitude","day")]
+temporal_variables <- temporal_variables[complete.cases(temporal_variables), ]
+chart.Correlation(temporal_variables, histogram=TRUE, pch=19)
+
 
 env_vars <- km_df_filtered[, c("TSM", "CUR", "SST","CHLA", "Tmin", "Tmax", "O2_min", "SML", "Smax", "lunar_fraction", "moon_phase", "altitude")]
 env_vars <- env_vars[complete.cases(env_vars), ]
@@ -32,9 +57,8 @@ km_sf <- km_sf[km_sf$tax.grp %in% include_taxa, ]
 env_vars <- km_sf[, c("bm_log" , "TSM", "CUR", "SST","CHLA", "Tmin", "Tmax", "O2_min", "SML", "Smax", "lunar_fraction", "moon_phase", "altitude" )]
 env_vars <- env_vars[complete.cases(env_vars), ]
 
-chart.Correlation(env_vars, histogram=TRUE, pch=19)
+chart.Correlation(temporal_variables, histogram=TRUE, pch=19)
 
-km_df_filtered$bm_log <- log(km_df_filtered$bm_g_m3)
 
 
 
