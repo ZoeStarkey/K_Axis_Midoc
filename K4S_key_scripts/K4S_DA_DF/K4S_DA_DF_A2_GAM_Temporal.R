2011e7aab3060064f5dcedef6e2f6f378a500fff
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-08-08 12:46:00 +1000

    K4S_DA_DF_A2_GAM_Temporal: adding in the depth structured dataframe

--- K4S_key_scripts/K4S_DA_DF/K4S_DA_DF_A2_GAM_Temporal.R
+++ K4S_key_scripts/K4S_DA_DF/K4S_DA_DF_A2_GAM_Temporal.R
@@ -10,11 +10,12 @@ dir.exists(d)
 load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/km_df_environmental_variables.Rda")
 
 # Define the depth ranges for each codend
-depth_bins <- c("0-1000", "800-1000", "600-800", "400-600", "200-400", "0-200")
+depth_bins <- c("0-200", "200-400", "400-600", "600-800", "800-1000", "0-1000")
 
 # Map the codends to depth ranges using the factor function
-km_df$depth <- factor(km_df$cod.end, levels = c("1", "2", "3", "4", "5", "6"), labels = depth_bins)
-
+km_df$depth <- factor(km_df$cod.end, 
+                      levels = c("6", "5", "4", "3", "2", "1"), 
+                      labels = depth_bins)
 
 #removing 0-1000m
 remove_depth <- c("0-1000")
@@ -34,8 +35,7 @@ km_df <- km_df %>%
 #delete the columns not in use 
 km_df <- subset(km_df, select = -azimuth)
 km_df <- subset(km_df, select = -zone.notes)
-km_df <- subset(km_df, select = -depth)
-km_df<- subset(km_df, select = -cod.end)
+
 
 #adding a day column
 
@@ -55,12 +55,6 @@ exclude_taxa <- c("cnidarians", "salps", "mixed/other gelatinous", "mixed krill 
 # Filter the dataframe to exclude the specified taxa and only include non-NA depths
 km_filtered_all_taxa <- km_df %>%
   filter(!tax.grp %in% exclude_taxa) 
-
-new_df <- km_df %>%
-  dplyr::select(midoc.stn, start_time, date, day_numeric) %>%
-  arrange(midoc.stn, start_time)
-
-
 
 #sum across the dataframe
 km_sum_all_taxa <- km_filtered_all_taxa %>%
@@ -106,6 +100,8 @@ km_bm_sum <- subset(km_bm_sum, select = -tax.grp)
 km_bm_sum <- subset(km_bm_sum, select = -fish.grp)
 km_bm_sum <- subset(km_bm_sum, select = -bm_g_m3)
 km_bm_sum <- subset(km_bm_sum, select = - swept_m3)
+km_bm_sum <- subset(km_bm_sum, select = - depth)
+km_bm_sum <- subset(km_bm_sum, select = - cod.end)
 
 #reordering so the biomass columns are next to each other 
 km_bm_sum <-km_bm_sum %>%
@@ -123,5 +119,55 @@ load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/km_
 
 
 
+### SUMMING ACCROSS MIDOC AND DEPTH 
+exclude_taxa <- c("cnidarians", "salps", "mixed/other gelatinous", "mixed krill and salps", "mixed/other invertebrates")
 
+km_bm_all_taxa_sum_depth <- km_df %>%
+  filter(!tax.grp %in% exclude_taxa) %>%
+  group_by(midoc.stn, depth) %>%
+  summarize(
+    bm_depth_all_taxa = sum(bm_g_m3, na.rm = TRUE),
+    across(everything(), ~first(.)),
+    .groups = "drop"
+  )
+
+#FISH
+include_taxa <- c("fish")
+km_bm_fish_sum_depth <- km_df %>%
+  filter(tax.grp %in% include_taxa) %>%
+  group_by(midoc.stn, depth) %>%
+  summarize(
+    bm_depth_fish = sum(bm_g_m3, na.rm = TRUE),
+    across(everything(), ~first(.)),
+    .groups = "drop"
+  )
+
+#SQUID
+include_taxa <- c("cephalopods")
+km_bm_squid_sum_depth <- km_df %>%
+  filter(tax.grp %in% include_taxa) %>%
+  group_by(midoc.stn, depth) %>%
+  summarize(
+    bm_depth_ceph = sum(bm_g_m3, na.rm = TRUE),
+    across(everything(), ~first(.)),
+    .groups = "drop"
+  )
+
+km_bm_depth <- km_bm_all_taxa_sum_depth %>%
+  left_join(km_bm_fish_sum_depth, by = c("midoc.stn", "depth")) %>%
+  left_join(km_bm_squid_sum_depth, by = c("midoc.stn", "depth"))
+
+km_bm_depth <-km_bm_depth %>%
+  dplyr::select(
+    midoc.stn,
+    bm_depth_all_taxa,
+    bm_depth_fish,
+    bm_depth_ceph,
+    everything()
+  )
+
+
+
+save(km_bm_depth , file = "km_bm_depth.Rda")
+load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/km_bm_depth.Rda")
 
