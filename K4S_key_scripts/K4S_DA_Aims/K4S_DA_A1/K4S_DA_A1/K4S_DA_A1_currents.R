b60a3dc07e55a20dadd6ec860ea2511edd2e2a58
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-08-14 14:17:24 +1000

    K4S_DA_A1_CS: adjusting axis title size and legends to prepare for presentation. changed colour of voyage track, moved fronts above ice so that it doesnt plot over the ice layer

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A1/K4S_DA_A1/K4S_DA_A1_currents.R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A1/K4S_DA_A1/K4S_DA_A1_currents.R
@@ -157,7 +157,8 @@ km_sf_total <- km_sf_total %>%
                            include.lowest = TRUE))
 
 # Now create the plot
-CS_total <- ggplot() +
+CS_total <- 
+  ggplot() +
   # Add the base raster layer for current speed
   geom_raster(data = mn_mag_df, aes(x = x, y = y, fill = value), interpolate = TRUE) +
   scale_fill_gradientn(colors = cols2(100), 
@@ -167,7 +168,16 @@ CS_total <- ggplot() +
                                               title.hjust = 0.5,
                                               label.position = "right",
                                               barwidth = 1,
-                                              barheight = 8)) +
+                                              barheight = 16,
+                                              order = 2,
+                                              frame.linewidth = 0.2,
+                                              # ticks.linewidth = 0.5,
+                                              title.theme = element_text(size = 14, angle = 90),
+                                              label.theme = element_text(size = 14))) +
+  
+  # Add f3$finished and f1$finished
+  geom_sf(data = f3$finished, color = "black", linewidth = 1) +
+  geom_sf(data = f1$finished, color = "black", linewidth = 1) +
   # Add ice
   ggnewscale::new_scale_fill() + 
   geom_tile(data = ice_df, aes(x = x, y = y, fill = k.axis_data_ICE_LONGLAT_20160218), alpha = 0.8) +
@@ -177,11 +187,12 @@ CS_total <- ggplot() +
                                               title.hjust = 0.5,
                                               label.position = "right",
                                               barwidth = 1,
-                                              barheight = 8)) +
-  
-  # Add f3$finished and f1$finished
-  geom_sf(data = f3$finished, color = "black", linewidth = 1) +
-  geom_sf(data = f1$finished, color = "black", linewidth = 1) +
+                                              barheight = 8,
+                                              order = 3,
+                                              frame.linewidth = 0.2,
+                                              #ticks.linewidth = 0.5,
+                                              title.theme = element_text(size = 14, angle = 90),
+                                              label.theme = element_text(size = 14))) +
   
   ggnewscale::new_scale_fill() +
   geom_sf(data = wcp_sf, fill = NA, color = "black") +
@@ -189,7 +200,7 @@ CS_total <- ggplot() +
   geom_sf(data = wp_sf, fill = "dark grey", color = NA) +
   annotate("segment", x = xx, xend = xx, y = min(yy), yend = max(yy), color = "gray40", linetype = "dashed") +
   annotate("segment", y = yy, yend = yy, x = min(xx), xend = max(xx), color = "gray40", linetype = "dashed") +
-  geom_sf(data = ktr_sf, size = 1) +
+  geom_sf(data = ktr_sf, size = 1, colour = "grey30") +
   geom_sf(data = km_sf_total, aes(fill = biomass_bin, size = biomass_bin), shape = 21, color = "black") +
   scale_fill_manual(
     values = c("white", "grey65", "grey30", "black"),
@@ -207,29 +218,29 @@ CS_total <- ggplot() +
     legend.background = element_blank(),
     legend.key = element_blank(),
     legend.title = element_text(angle = 90, hjust = 0.5),
+    legend.text = element_text(size = 14),
     legend.box.background = element_blank(),
     legend.byrow = TRUE,
-    strip.background = element_rect(fill = "white")
+    strip.background = element_rect(fill = "white"),
+    axis.title = element_text(size = 20),  # Increased axis title size
+    axis.text = element_text(size = 16),
+    axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
+    axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
+    plot.margin = margin(t = 10, r = 10, b = 10, l = 10, unit = "pt")
   ) +
   guides(
     fill = guide_legend(
       title.position = "left", 
       title.hjust = 0.5,
       override.aes = list(size = c(6, 8, 10, 12)),
-      order = 1
+      order = 1,
+      title.theme = element_text(size = 14, angle = 90)
     ),
     size = guide_legend(
       title.position = "left", 
       title.hjust = 0.5,
-      order = 1
-    ),
-    colour = guide_colorbar(
-      title.position = "left", 
-      title.hjust = 0.5,
-      label.position = "right",
-      barwidth = 1,
-      barheight = 8,
-      order = 2
+      order = 1,
+      title.theme = element_text(size = 14, angle = 90)
     )
   ) +
   theme(
