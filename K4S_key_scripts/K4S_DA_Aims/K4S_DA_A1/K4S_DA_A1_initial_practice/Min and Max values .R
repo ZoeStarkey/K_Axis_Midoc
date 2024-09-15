d471a554123125b70045304e35ad8270002bc038
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-09-15 22:48:11 +1000

    K4S_DA_A1_Min_max: just a dump script to check min and max values for the results section

--- K4S_key_scripts/K4S_DA_Aims/K4S_DA_A1/K4S_DA_A1_initial_practice/Min and Max values .R
+++ K4S_key_scripts/K4S_DA_Aims/K4S_DA_A1/K4S_DA_A1_initial_practice/Min and Max values .R
@@ -39,6 +39,7 @@ max_row$midoc.stn
 max(km_bm_sum$CHLA, na.rm = TRUE)
 min(km_bm_sum$CHLA, na.rm = TRUE)
 
+max(km_bm_sum$bm_sum_all_taxa, na.rm = TRUE)
 max(km_bm_sum$TSM, na.rm = TRUE)
 min(km_bm_sum$TSM, na.rm = TRUE)
 
@@ -51,4 +52,69 @@ min(km_bm_sum$CUR, na.rm = TRUE)
 max(km_bm_sum$altitude, na.rm = TRUE)
 min(km_bm_sum$altitude, na.rm = TRUE)
 
+#summing the total krill across all midoc stations 
+sum(km_bm_sum$bm_sum_krill, na.rm = TRUE)
+sum(km_bm_sum$bm_sum_all_taxa, na.rm = TRUE)
+sum(km_bm_sum$bm_sum_fish, na.rm = TRUE)
+
+
+load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/K4S_DA_DF/km_bm_depth.Rda")
+#summing the krill across the 0-200m range 
+sum(km_bm_depth$bm_depth_krill, na.rm = TRUE)
+
+result <- km_bm_depth %>%
+  group_by(depth) %>%
+  summarise(total_krill_biomass = sum(bm_depth_krill, na.rm = TRUE)) %>%
+  mutate(percentage = total_krill_biomass / sum(total_krill_biomass) * 100) %>%
+  arrange(depth)
+
+sum(km_bm_depth$bm_depth_krill, na.rm = TRUE)
+sum(result$total_krill_biomass)
+
+barplot(result$total_krill_biomass,
+        names.arg = result$depth, 
+        main = "Krill Biomass by Depth", 
+        xlab = "Depth Category", 
+        ylab = "Total Biomass")
+
+#how many of each character type in DNC.visual
+table(km_bm_sum$DNC.visual)
+
+load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/K4S_DA_DF/km_bm_sum.Rda")
+
+
+#ALL TAXA 
+biomass_summary <- km_bm_sum %>%
+  filter(DNC.visual %in% c("NC", "N", "MC", "D")) %>%
+  group_by(DNC.visual) %>%
+  summarise(total_biomass = sum(bm_sum_all_taxa, na.rm = TRUE)) %>%
+  mutate(period = case_when(
+    DNC.visual == "NC" ~ "Sunset",
+    DNC.visual == "N" ~ "Night",
+    DNC.visual == "MC" ~ "Sunrise",
+    DNC.visual == "D" ~ "Day"
+  )) %>%
+  dplyr::select(period, total_biomass)
+
+print(biomass_summary)
+
+biomass_summary_fish <- km_bm_sum %>%
+  filter(DNC.visual %in% c("NC", "N", "MC", "D")) %>%
+  group_by(DNC.visual) %>%
+  summarise(total_biomass_fish = sum(bm_sum_fish, na.rm = TRUE)) %>%
+  mutate(period = case_when(
+    DNC.visual == "NC" ~ "Sunset",
+    DNC.visual == "N" ~ "Night",
+    DNC.visual == "MC" ~ "Sunrise",
+    DNC.visual == "D" ~ "Day"
+  )) %>%
+  dplyr::select(period, total_biomass_fish)
+
+print(biomass_summary_fish)
+
+
+
+#sum of day and night 0.1913
+ 
+ sum(km_bm_sum$ bm_sum_all_taxa, na.rm = TRUE)
 
