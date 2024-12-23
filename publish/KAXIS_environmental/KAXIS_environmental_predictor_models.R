ecfbcd21527a7c07b13ecc79fcec81fdfb66c7da
ZoeStarkey <zoe.starkey@utas.edu.au>
2024-12-23 13:08:43 +1100

    Initial commit of the environmental predictor models for publish

---
+++ publish/KAXIS_environmental/KAXIS_environmental_predictor_models.R
@@ -0,0 +1,53 @@
+
+library(dplyr)
+library(mgcv)
+library(gamm4)
+library(gratia)
+library(patchwork)
+library(ggplot2)
+library(grid)
+
+
+
+################ADDITIVE MODELS SATELLITE ALL VARIABLES##########################
+load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/K4S_DA_DF/km_bm_sum_2.Rda")
+km_bm_sum_2 <- km_bm_sum_2 %>% filter(SST > 0)
+
+#km_bm_sum_2 <-  km_bm_sum_2 %>% filter(days_since_melt <500)
+
+#days since melt is unusually high for MIDOC 27, averaging the results from nearby stations 
+km_bm_sum_2_filtered <- km_bm_sum_2_filtered %>% filter(midoc.stn == "MIDOC26" | midoc.stn == "MIDOC28")
+#average of 110.00001 days since ice melt (from MIDOC26) and 84.99997 days since ice melt (from MIDOC 28)
+(110.00001+84.99997)/2
+#97.49999
+#input 97.49999 into the days since melt value for MIDOC27 in the km_bm_sum_2 
+km_bm_sum_2$days_since_melt[km_bm_sum_2$midoc.stn == "MIDOC27"] <- 97.49999
+
+
+allbiom_additive_all_vars <-gam(log(bm_sum_all_taxa) ~ s(SST)+ s(CUR) + s(chl_rs) +s(days_since_melt), data = km_bm_sum_2)
+draw(allbiom_additive_all_vars, residuals = TRUE) +
+  #ggtitle("Biomass (Log) All Taxa Additive model all vars") + 
+  theme(plot.title = element_text(hjust = -8, vjust = 9 ))
+summary(allbiom_additive_all_vars)
+gam.check(allbiom_additive_all_vars)
+
+
+fish_additive_all_vars <-gam(log(bm_sum_fish) ~ s(SST)+ s(CUR) + s(chl_rs) +s(days_since_melt), data = km_bm_sum_2)
+draw(fish_additive_all_vars, residuals = TRUE) 
+#+ ggtitle("Fish Biomass (Log) All Taxa Additive model all vars") + 
+theme(plot.title = element_text(hjust = 4, vjust = 9 ))
+summary(fish_additive_all_vars)
+gam.check(fish_additive_all_vars)
+
+ceph_additive_all_vars <-gam(log(bm_sum_ceph) ~ s(SST)+ s(CUR) + s(chl_rs) +s(days_since_melt), data = km_bm_sum_2)
+draw(ceph_additive_all_vars, residuals = TRUE) 
+#+ ggtitle("Cephalopod Biomass (Log) All Taxa Additive model all vars") + 
+# theme(plot.title = element_text(hjust = -7.5, vjust = -20 ))
+summary(ceph_additive_all_vars)
+gam.check(ceph_additive_all_vars)
+
+krill_additive_all_vars <-gam(log(bm_sum_krill) ~ s(SST)+ s(CUR) + s(chl_rs) +s(days_since_melt), data = km_bm_sum_2)
+draw(krill_additive_all_vars, residuals = TRUE) + ggtitle("Krill Biomass (Log) All Taxa Additive model all vars") + 
+  theme(plot.title = element_text(hjust = -7.5, vjust = -20 ))
+summary(krill_additive_all_vars)
+gam.check(krill_additive_all_vars)
