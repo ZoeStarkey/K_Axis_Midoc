5f3c091f639d34597c0ca62bd1584f42c4efebac
ZoeStarkey <zoe.starkey@utas.edu.au>
2025-05-21 13:06:24 +1000

    Changed so that day fractional was created from the km_bm_depth_2 dataset, not the km_bm_sum_2 dataset

--- publish/KAXIS_temporal/KAXIS_temporal_depth_structured_models.R
+++ publish/KAXIS_temporal/KAXIS_temporal_depth_structured_models.R
@@ -13,7 +13,7 @@ library(lubridate)
 load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/K4S_DA_DF/km_bm_depth_2.Rda")
 
 #making "day" fractional
-with(km_bm_sum_2, plot(day,lunar_fraction))
+with(km_bm_depth_2, plot(day,lunar_fraction))
 
 #adding day_fraction to km_bm_depth
 km_bm_depth_2$day_of_year <- yday(km_bm_depth_2$start_time)
@@ -31,7 +31,7 @@ summary(fish_depth.day.re$lme)
 gam.check(fish_depth.day.re$gam)
 
 #solar angle
-fish_depth.solar.re <- gamm(log(bm_depth_fish) ~ depth + s(altitude, by = depth),data = km_bm_depth, random = list(midoc.stn = ~ 1 ))
+fish_depth.solar.re <- gamm(log(bm_depth_fish) ~ depth + s(altitude, by = depth),data = km_bm_depth_2, random = list(midoc.stn = ~ 1 ))
 fish_depth_solar <- draw(fish_depth.solar.re, residuals = TRUE) + theme(plot.margin = margin(t = 30, r = 20, b = 40, l = 20, unit = "pt"))
 #grid.text("Biomass (Logged) by depth + RE: Fish - Solar Angle",  x = unit(0.19, "npc"), y = unit(0.05, "npc"), just = c("left", "bottom" ),  gp = gpar(fontsize = 14, fontface = "bold"))
 summary(fish_depth.solar.re$gam)
