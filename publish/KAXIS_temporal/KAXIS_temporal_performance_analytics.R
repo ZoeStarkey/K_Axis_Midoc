b04fef54759c7adbd0601de34c537221c8354a76
ZoeStarkey <zoe.starkey@utas.edu.au>
2025-05-21 13:34:24 +1000

    Changing it from km_bm_depth_2 to km_bm_sum_2. also added in lubridate library for fractional date

--- publish/KAXIS_temporal/KAXIS_temporal_performance_analytics.R
+++ publish/KAXIS_temporal/KAXIS_temporal_performance_analytics.R
@@ -3,35 +3,43 @@ library(ggplot2)
 library(mgcv)
 library(caret)
 library(dplyr)
+library(lubridate)
 
 
 #load the data
-load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/K4S_DA_DF/km_bm_depth_2.Rda")
+load("~/Desktop/Honours/Data_Analysis/K_axis_midoc/K4S_key_scripts/K4S_DA_DF/K4S_DA_DF/km_bm_sum_2.Rda")
 
 #making "day" fractional
-with(km_bm_depth_2, plot(day,lunar_fraction))
+with(km_bm_sum_2, plot(day,lunar_fraction))
 
-#adding day_fraction to km_bm_depth
-km_bm_depth_2$day_of_year <- yday(km_bm_depth_2$start_time)
-km_bm_depth_2$day_fraction <- km_bm_depth_2$day_of_year + hour(km_bm_depth_2$start_time) / 24
+#adding day_fraction to km_bm_sum
+km_bm_sum_2$day_of_year <- yday(km_bm_sum_2$start_time)
+km_bm_sum_2$day_fraction <- km_bm_sum_2$day_of_year + hour(km_bm_sum_2$start_time) / 24
 
 #Temporal performance analytics for all taxa
-temporal_PA_all_taxa <- km_bm_depth_2[, c("bm_depth_all_taxa", "lunar_fraction", "altitude", "day_fraction")]
-temporal_PA_all_taxa$bm_depth_all_taxa <- log(temporal_PA_all_taxa$bm_depth_all_taxa)
+temporal_PA_all_taxa <- km_bm_sum_2[, c("bm_sum_all_taxa", "lunar_fraction", "altitude", "day_fraction")]
+temporal_PA_all_taxa$bm_sum_all_taxa <- log(temporal_PA_all_taxa$bm_sum_all_taxa)
 temporal_PA_all_taxa <- temporal_PA_all_taxa[complete.cases(temporal_PA_all_taxa), ]
 #plot
 chart.Correlation(temporal_PA_all_taxa, histogram=TRUE, pch=19, )
 
 #Temporal performance analytics for fish
-temporal_PA_fish <- km_bm_depth_2[, c("bm_depth_fish", "lunar_fraction", "altitude", "day_fraction")]
-temporal_PA_fish$bm_depth_fish <- log(temporal_PA_fish$bm_depth_fish)
+temporal_PA_fish <- km_bm_sum_2[, c("bm_sum_fish", "lunar_fraction", "altitude", "day_fraction")]
+temporal_PA_fish$bm_sum_fish <- log(temporal_PA_fish$bm_sum_fish)
 temporal_PA_fish <- temporal_PA_fish[complete.cases(temporal_PA_fish), ]
 #plot
 chart.Correlation(temporal_PA_fish, histogram=TRUE, pch=19, )
 
 #Temporal performance analytics for cephalopods
-temporal_PA_ceph <- km_bm_depth_2[, c("bm_depth_ceph", "lunar_fraction", "altitude", "day_fraction")]
-temporal_PA_ceph$bm_depth_ceph <- log(temporal_PA_ceph$bm_depth_ceph)
+temporal_PA_ceph <- km_bm_sum_2[, c("bm_sum_ceph", "lunar_fraction", "altitude", "day_fraction")]
+temporal_PA_ceph$bm_sum_ceph <- log(temporal_PA_ceph$bm_sum_ceph)
 temporal_PA_ceph <- temporal_PA_ceph[complete.cases(temporal_PA_ceph), ]
 #plot
 chart.Correlation(temporal_PA_ceph, histogram=TRUE, pch=19, )
+
+#Temporal performance analytics for krill
+temporal_PA_krill <- km_bm_sum_2[, c("bm_sum_krill", "lunar_fraction", "altitude", "day_fraction")]
+temporal_PA_krill$bm_sum_krill <- log(temporal_PA_krill$bm_sum_krill)
+temporal_PA_krill <- temporal_PA_krill[complete.cases(temporal_PA_krill), ]
+#plot
+chart.Correlation(temporal_PA_krill, histogram=TRUE, pch=19, )
