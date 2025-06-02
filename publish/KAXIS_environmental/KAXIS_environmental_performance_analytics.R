1a15f4225febd07c984b7bc9ccd64b4721f26ac5
ZoeStarkey <zoe.starkey@utas.edu.au>
2025-06-02 15:19:10 +1000

    adding a function for the heatmap

--- publish/KAXIS_environmental/KAXIS_environmental_performance_analytics.R
+++ publish/KAXIS_environmental/KAXIS_environmental_performance_analytics.R
@@ -42,4 +42,59 @@ env_PA_krill <- env_PA_krill[complete.cases(env_PA_krill), ]
 #plot
 chart.Correlation(env_PA_krill, histogram=TRUE, pch=19, )
 
+#############plot heatmap############
+plot_ggcorr <- function(data, var_labels) {
+  cor_matrix <- cor(data, use = "complete.obs")
+  
+  # Rename columns and rows
+  colnames(cor_matrix) <- var_labels
+  rownames(cor_matrix) <- var_labels
+  
+  ggcorrplot(cor_matrix,
+             method = "square",
+             type = "lower",
+             lab = TRUE,
+             lab_size = 6,
+             lab_col = "black",
+             tl.cex = 12,
+             show.legend = TRUE) +
+    scale_fill_gradient2(
+      low = "blue", mid = "white", high = "red",
+      midpoint = 0,
+      limits = c(-1, 1),
+      name = "Correlation Coefficient",
+      guide = guide_colorbar(
+        barheight = 35,
+        title.position = "left",   
+        label.position = "right"
+      )
+    ) +
+    theme(
+      axis.text.x = element_text(size = 16, colour = 'black'),
+      axis.text.y = element_text(size = 16, colour = 'black'),
+      plot.title = element_text(hjust = 0.5, face = "bold", size = 20),
+      legend.title = element_text(size = 16, hjust = 0.5, face = "bold", angle = 90),
+      legend.text  = element_text(size = 16)
+    )
+}
+
+# ---- Variable labels for environmental data ----
+env_labels <- c("Biomass", "TSM", "Current speed", "SST", "Chl–a")
+
+# ---- Create plots for each group ----
+p_all_env   <- plot_ggcorr(env_PA_all_taxa, env_labels) + ggtitle("Total Biomass")
+p_fish_env  <- plot_ggcorr(env_PA_fish,     env_labels) + ggtitle("Fish")
+p_ceph_env  <- plot_ggcorr(env_PA_ceph,     env_labels) + ggtitle("Cephalopods")
+p_krill_env <- plot_ggcorr(env_PA_krill,    env_labels) + ggtitle("Krill")
+
+# ---- Combine the plots into a 2x2 grid ----
+env_PA_heatmap <- (p_all_env + p_fish_env) / (p_ceph_env + p_krill_env) +
+  plot_layout(guides = "collect") & theme(legend.position = "right")
+
+env_PA_heatmap
+
+# Save the combined plot
+ggsave("KAXIS_environmental_performance_analytics.png", 
+       plot = env_PA_heatmap, 
+       width = 12, height = 10, dpi = 300) 
 
